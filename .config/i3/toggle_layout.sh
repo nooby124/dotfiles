dace4ed2bb9643fded6e4a683b5c9e6df1f883b5
nooby124 <amitay33126@gmail.com>
2024-08-25 12:52:58 +0300

    update i3 config for playerctl and changed caps lock to hyper

--- .config/i3/toggle_layout.sh
+++ .config/i3/toggle_layout.sh
@@ -5,10 +5,10 @@ current_layout=$(setxkbmap -query | grep layout | awk '{print $2}')
 
 # Toggle between US, Hebrew, and Estonian
 if [ "$current_layout" = "us" ]; then
-    setxkbmap il
+    setxkbmap il -option "caps:super"
 elif [ "$current_layout" = "il" ]; then
-    setxkbmap ee
+    setxkbmap ee -option "caps:super"
 else
-    setxkbmap us
+    setxkbmap us -option "caps:super"
 fi
 
