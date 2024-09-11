87ea0f114ef27703eba93637056662d29e429535
nooby124 <amitay33126@gmail.com>
2024-09-11 13:03:26 +0300

    nvim go brr

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 require("config.lazy")
 require("mason").setup()
+require("autoclose").setup()
 
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.ast_grep.setup{}
@@ -8,7 +9,17 @@ require'lspconfig'.lua_ls.setup{}
 require'lspconfig'.html.setup{}
 require'lspconfig'.bashls.setup{}
 
+vim.keymap.set('n', '<A-w>', '<C-w>w', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<CR>', [[pumvisible() ? "\<C-y>" : "\<CR>"]], { noremap = true, silent = true, expr = true })
+
+vim.api.nvim_set_keymap("n", "<C-n>", ":Neotree toggle right<CR>", { noremap = true, silent = true })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = "Recent Files" })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Grep Files" })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find Buffers" })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Find Help" })
 
 
 vim.cmd("colorscheme carbonfox")
--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,9 +2,18 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- optional, for file icons
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                window = {
+                    position = "right", -- Set Neo-tree to open on the right
+                },
+            },
+        })
+    end,
 }
+
