d890b6fba6982ca398498b68cf8a1b924868f33c
nooby124 <amitay33126@gmail.com>
2024-09-13 23:44:54 +0300

    added aliases

---
+++ .aliases.zsh
@@ -0,0 +1,20 @@
+
+alias ff="fzf --preview='bat --color always {}'"
+alias ":q!"="exit"
+alias filesize="du -bsh"
+alias dirsize="du -sh"
+alias cat="bat"
+alias f="fuck"
+#alias lf=lf-ueberzug
+alias vesktop="flatpak run dev.vencord.Vesktop"
+alias freetube="flatpak run io.freetubeapp.FreeTube"
+#alias lf="lf --command 'set hidden'"
+alias pipes=pipes.sh
+alias archbtw="i-use-arch-btw"
+#fastfetch
+alias ls="ls -A"
+alias cd=z
+alias i3config="nvim ~/.config/i3/config"
+alias c="xsel --input --clipboard"
+alias p="xsel --output --clipboard" 
+alias neofetch="fastfetch -l ~/aperture.txt --logo-color-1 yellow"
